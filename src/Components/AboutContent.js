1cd2e3e8129702d63655339aab8b60bb308cd525
priyanshu1120 <priyanshukaushik796@gmail.com>
2022-09-13 20:04:48 +0530

    cv download

--- src/Components/AboutContent.js
+++ src/Components/AboutContent.js
@@ -1,6 +1,6 @@
 import "./AboutContent.css"
  import React from 'react'
- import {Link} from "react-router-dom"
+
  
  const AboutContent = () => {
    return (
@@ -8,7 +8,7 @@ import "./AboutContent.css"
        <div className="left" >
         <h1>Who Am I ?</h1>
         <p>Hello! I am Priyanshu Kaushik a Fullstack developer, I am a consistent learner who believe in hard work . I am most attracted to solving real customer problems with a business justification, I am having good experience in computer fundamentals and love to do coding.</p>
-        <Link to="/contect"><button className="Btn">Download CV</button></Link>
+        <a href="./priyanshu_kaushik_resume.pdf" download><button className="Btn">Download CV</button></a>
        </div>
 
      <div className="right">
