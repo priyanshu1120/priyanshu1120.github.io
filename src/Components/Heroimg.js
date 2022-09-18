5836cee91f7194e6a84737d04f18268c89389a4e
priyanshu1120 <priyanshukaushik796@gmail.com>
2022-09-18 13:10:48 +0530

    name edit

--- src/Components/Heroimg.js
+++ src/Components/Heroimg.js
@@ -13,10 +13,10 @@ const Heroimg = () => {
         </div>
         <div className="content">
             <img src="profile.jpg" alt="profile" className="profileimg" />
-            <h2 style={{ marginBottom:"40px"}}>Hi I'M PRIYANSHU KAUSHIK</h2>
+            <h2 style={{ marginBottom:"40px"}}>Hi I'M <span style={{color:"yellow" ,fontSize:"1.8rem"}}>PRIYANSHU KAUSHIK</span></h2>
             <h4 style={{ marginTop:"-60px"}}>FRONT END DEVELOPER</h4>
             <div className="btn-box">
-               <Link to="/project" className="Btn">PROJECTS</Link>  
+               <a href="./priyanshu_kaushik_resume.pdf" download className="Btn">RESUME</a>  
                <Link to="/contect" className="Btn Btn-light">Contact</Link>   
             </div>
         </div>
